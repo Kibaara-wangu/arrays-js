df93a031a47ce69935be888e07e6becc485cd48f
kibaara-wangu <kibaarawangu@gmail.com>
2024-03-29 19:46:50 +0300

    added functions

---
+++ Arrays.js
@@ -0,0 +1,33 @@
+// Given an array of strings, use a function to reverse all the elements in the string in ascending order and the specific elements in descending order
+function sorting(){
+let names = ["Kamau","Maina","Rebecca","Anita","Joy"];
+console.log(names.sort());
+console.log(names.reverse());
+}
+sorting()
+
+
+
+
+
+
+// Using JS functions and an array of numbers, return positive if an element within the array is positive, negative if an element is negative, else zero
+function elementCheck(){
+let number= number.forEach(element => {
+    if (element >0){
+        console.log("positive");
+    }
+    else if (element < 0){
+        console.log("negative");
+    }else{
+        console.log("zero");
+    }
+});
+
+var numbers = [4,5,7,8]
+var numberif
+}
+
+
+
+
